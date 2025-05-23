bedbaf2d2bfa01b34f6bdfba7fe85bd1b311d1c3
Alan Luis Valenzuela Simpson <alan.v.simpson@hotmail.com>
2025-05-23 17:55:51 +0200

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -56,9 +56,18 @@ export class AppModule {
   static port: number | string;
 
   constructor(private readonly _configService: ConfigService) {
-    console.log('Configuration.PORT', Configuration.PORT);
-    console.log('Configuration.BASE_PATH', Configuration.BASE_PATH);
-    console.log('Configuration.APP_HOST', Configuration.APP_HOST);
+    console.log(
+      'Configuration.PORT',
+      this._configService.get(Configuration.PORT),
+    );
+    console.log(
+      'Configuration.BASE_PATH',
+      this._configService.get(Configuration.BASE_PATH),
+    );
+    console.log(
+      'Configuration.APP_HOST',
+      this._configService.get(Configuration.APP_HOST),
+    );
 
     AppModule.port = this._configService.get(Configuration.PORT);
     AppModule.basePath = this._configService.get(Configuration.BASE_PATH);
