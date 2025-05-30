324c575b8705fc2a427de3c919c73969d343a92e
Alan Luis Valenzuela Simpson <alan.v.simpson@hotmail.com>
2025-05-30 14:56:04 +0200

    test

--- src/interceptors/log/log.interceptor.ts
+++ src/interceptors/log/log.interceptor.ts
@@ -5,26 +5,49 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { Observable, tap } from 'rxjs';
+import { Observable, tap, catchError } from 'rxjs';
 import { logger } from './logger';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
+    const method = request.method;
+    const url = request.url;
+    const body = request.body ?? {};
+    const query = request.query ?? {};
+    const params = request.params ?? {};
 
-    const { method, url } = request;
-    const now = Date.now();
+    const microservice = 'paint-microservice';
+    const startTimestamp = new Date().toISOString();
+    const startTime = Date.now();
+
+    if (url === '/auth/health-check') {
+      return next.handle();
+    }
+
+    logger.info(
+      `[${microservice}] [START] ${method} ${url} [Query] ${JSON.stringify(query)} [Params] ${JSON.stringify(params)} [Body] ${JSON.stringify(body)} [Timestamp] ${startTimestamp}`,
+    );
 
     return next.handle().pipe(
-      tap((data) => {
-        const delay = Date.now() - now;
-        logger.info(`Handled ${method} ${url} in ${delay}ms`, {
-          method,
-          url,
-          delay,
-          response: data,
-        });
+      tap((response) => {
+        const endTime = Date.now();
+        const delay = endTime - startTime;
+        const endTimestamp = new Date().toISOString();
+        logger.info(
+          `[${microservice}] [END] ${method} ${url} [Response] ${JSON.stringify(response)} [Processing Time] ${delay}ms [Timestamp] ${endTimestamp}`,
+        );
+      }),
+      catchError((error) => {
+        console.info('error', error);
+        const endTime = Date.now();
+        const delay = endTime - startTime;
+        const errorTimestamp = new Date().toISOString();
+        logger.error(
+          `[${microservice}] [ERROR] ${method} ${url} [Message] ${error.message} [Processing Time] ${delay}ms [Timestamp] ${errorTimestamp}`,
+        );
+        throw error;
       }),
     );
   }
--- src/interceptors/log/logger.ts
+++ src/interceptors/log/logger.ts
@@ -6,33 +6,41 @@ import { Configuration } from '../../config/utils/config.keys';
 
 const configService = new ConfigService();
 const DATADOG_API_KEY = configService.get(Configuration.DATADOG_API_KEY);
-const NODE_ENV = process.env.NODE_ENV || 'development';
-const SERVICE_NAME = process.env.SERVICE_NAME || 'paint-microservice';
+const BRANCH = configService.get(Configuration.BRANCH);
+const SERVICE_NAME = `${BRANCH}-paint-microservice`;
 
 if (!DATADOG_API_KEY) {
   console.warn(
     'DATADOG_API_KEY no está configurado. Los logs no se enviarán a Datadog.',
   );
 }
-console.log('DATADOG_API_KEY', DATADOG_API_KEY);
-console.log('NODE_ENV', NODE_ENV);
-console.log('SERVICE_NAME', SERVICE_NAME);
 
 const logger = createLogger({
-  level: 'info',
-  format: format.combine(format.timestamp(), format.json()),
-  defaultMeta: { service: SERVICE_NAME },
+  format: format.combine(
+    format.errors({ stack: true }),
+    format.metadata(),
+    format.timestamp(),
+    format.printf(
+      ({ timestamp, level, message }) =>
+        `${timestamp} [${level.toUpperCase()}]: ${message}`,
+    ),
+  ),
   transports: [new transports.Console()],
 });
 
 if (DATADOG_API_KEY) {
   logger.add(
     new datadogWinston({
-      host: 'http-intake.logs.datadoghq.eu',
+      hostname: SERVICE_NAME,
       apiKey: DATADOG_API_KEY,
       service: SERVICE_NAME,
       ddsource: 'nodejs',
-      ddtags: `env:${NODE_ENV}`,
+      ddtags: `env:${BRANCH},service:${SERVICE_NAME}`,
+      site: 'datadoghq.eu',
+      intakeRegion: 'eu',
+      logLevel: 'info',
+      handleExceptions: true,
+      handleRejections: true,
     }),
   );
 }
